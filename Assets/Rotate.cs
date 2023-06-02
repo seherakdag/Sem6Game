d2d6f1f6927d64e708ca4431eb962bf96e507c60
seherakdag <113117082+seherakdag@users.noreply.github.com>
2023-06-02 16:55:21 +0200

    new

--- Assets/Rotate.cs
+++ Assets/Rotate.cs
@@ -41,7 +41,7 @@ public class Rotate : MonoBehaviour
         for (int i = 0; i < 16; i++)
         {
             transform.Rotate(1.40625f, 0f, 0f);
-            yield return new WaitForSeconds(0.01f);
+            yield return new WaitForSeconds(0.001f);
 
         }
 
